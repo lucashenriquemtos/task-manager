b9548bd495764a4feb7e0542b866121406069bee
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-10-17 20:35:54 -0300

    Criando TarefaService

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TarefaService} from "./shared";
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    TarefaService
   ]
 })
 export class TarefasModule { }
